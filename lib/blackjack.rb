106c5ba3d066c0d304fe30ba963e92b76aee5f27
Maximo Bautista <max.bautista97@gmail.com>
2018-12-22 15:27:04 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -49,7 +49,8 @@ end
 # get every test to pass before coding runner below #
 #####################################################
 
-def runner(method)
+def runner(card_total)
+  card_total = card_total
   until card_total > 21
     welcome method
     initial_round method
