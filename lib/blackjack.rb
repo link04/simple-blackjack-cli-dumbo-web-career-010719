1fe39a39235caed724989bb40b747d7f1fb7d722
Maximo Bautista <max.bautista97@gmail.com>
2018-12-22 15:43:05 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -52,13 +52,13 @@ end
 def runner
     welcome
     initial_round
-  hit?(initial_round)
-     display_card_total(card_total)
+
+    
   card_total = hit?(initial_round)
    
   until card_total > 21
-    display_card_total(card_total)
     hit?(initial_round)
+    display_card_total(card_total)
   end
    end_game(card_total)
 end
