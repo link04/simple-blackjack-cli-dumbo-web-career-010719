bb95c9dc5b9ab09f16a5dd9c980ffcbaa831fa03
Maximo Bautista <max.bautista97@gmail.com>
2018-12-22 15:19:38 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -34,7 +34,7 @@ def hit?(card_total)
   if answer == "h"
     deal_card
   elsif answer == "s"
-    puts "Got it."
+    puts "Type 'h' to hit or 's' to stay"
   else
     invalid_command
     prompt_user
